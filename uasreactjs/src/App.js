06308689f361b3360db9a895c74537d08142a084
Axel <axelharyanto@outlook.com>
2020-07-24 01:03:42 +0700

    Membuat Login

--- uasreactjs/src/App.js
+++ uasreactjs/src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useReducer} from 'react';
+import { BrowserRouter, Switch, Redirect, Route} from 'react-router-dom';
+import MenuComp from './Component/MenuComp';
+import HomeComp from './Component/HomeComp';
+import LoginComponent from './Component/LoginComponent';
+import RegisterComp from './Component/RegisterComp';
+
+//Context
+export const AuthContext = createContext()
+
+//Inisiasi State
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: null
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      localStorage.setItem("user", JSON.stringify(action.payload.user))
+      localStorage.setItem("token", JSON.stringify(action.payload.token))
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+        token: action.payload.token
+      }
+
+    case "LOGOUT":
+      localStorage.clear()
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null
+      }
+    
+    default:
+      return state
+  }
+}
+
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <AuthContext.Provider value={{
+          state,
+          dispatch
+        }}>
+          <MenuComp />
+          {!state.isAuthenticated ?
+            <Redirect
+              to={{
+                pathname: "/"
+              }}
+            /> :
+            <Redirect
+              to={{
+                pathname: "/homepage"
+              }}
+            />
+          }
+
+          <Route exact path="/" component={LoginComponent}/>
+          <Route exact path="/homepage" component={HomeComp}/>
+          <Route exact path="/register" component={RegisterComp}/>
+        </AuthContext.Provider>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
