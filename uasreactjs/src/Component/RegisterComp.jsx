54d976c099d77ec5e97c83583bf71d4050284784
Axel <axelharyanto@outlook.com>
2020-07-25 00:29:02 +0700

    Property Web
    Membuat Input Register

--- uasreactjs/src/Component/RegisterComp.jsx
+++ uasreactjs/src/Component/RegisterComp.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment, PureComponent } from 'react'
-import { Button, Form, FormGroup, Label, Input, Container, Row, Col, CardImg, Alert } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Container, Row, Col, Alert } from 'reactstrap';
 import axios from 'axios'
 const api = 'http://localhost:3002'
 
@@ -8,10 +8,11 @@ export default class RegisterComp extends PureComponent {
         super(props)
         this.state = {
             username: '',
-            password: '',
-            //status:'2',
+            email: '',
+            nama_user: '',
+            password: '',            
             response: '',
-            color:'',
+            color: '',
             display:'none'
         }
     }
@@ -23,8 +24,10 @@ export default class RegisterComp extends PureComponent {
     register = () => {
         axios.post(api+'/api/v1/register',{
             username: this.state.username,
-            password: this.state.password
-            //status: this.state.status
+            email: this.state.email,
+            nama_user: this.state.nama_user,
+            password: this.state.password,
+
         }).then(json => {
             if(json.data.status === 200){
                 if(json.data.values === 'Berhasil Membuat User'){
@@ -49,7 +52,10 @@ export default class RegisterComp extends PureComponent {
             }
         })
         this.setState({username:''})
+        this.setState({email:''})
+        this.setState({nama_user:''})
         this.setState({password:''})
+        
     }
 
     render() {
@@ -59,19 +65,35 @@ export default class RegisterComp extends PureComponent {
                     {this.state.response}
                 </Alert>
                 <Form className="form">
-                    <Label for="Username">Username</Label>
+                    <Label for="username">Username</Label>
                     <FormGroup>
                         <Row>
                             <Col>
-                                <Input type="text" name="username" id="UsernameField" value={this.state.username} onChange={this.handleRegisChange} placeholder="Masukan username" />
+                                <Input type="text" name="username" id="UsernameBox" value={this.state.username} onChange={this.handleRegisChange} placeholder="Masukkan username" />
                             </Col>
                         </Row>
                     </FormGroup>
-                    <Label for="Password">Password</Label>
+                    <Label for="email">Email</Label>
                     <FormGroup>
                         <Row>
                             <Col>
-                                <Input type="password" name="password" id="PasswordField" value={this.state.password} onChange={this.handleRegisChange} placeholder="Masukan password" />
+                                <Input type="email" name="email" id="EmailBox" value={this.state.email} onChange={this.handleRegisChange} placeholder="Masukkan email" />
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                    <Label for="nama_user">Nama Lengkap</Label>
+                    <FormGroup>
+                        <Row>
+                            <Col>
+                                <Input type="text" name="nama_user" id="NamaLengkap" value={this.state.nama_user} onChange={this.handleRegisChange} placeholder="Masukkan nama lengkap anda" />
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                    <Label for="password">Password</Label>
+                    <FormGroup>
+                        <Row>
+                            <Col>
+                                <Input type="password" name="password" id="PasswordBox" value={this.state.password} onChange={this.handleRegisChange} placeholder="Masukkan password" />
                             </Col>
                         </Row>
                     </FormGroup>
