8f52f2f5966b932bf706a880cd2fbe7f813a7122
Axel <axelharyanto@outlook.com>
2020-07-27 17:16:35 +0700

    KuyProperty
    Bug Home When Login

--- uasreactjs/src/Component/HomeComp.jsx
+++ uasreactjs/src/Component/HomeComp.jsx
@@ -2,16 +2,19 @@ import React, { useContext } from 'react'
 import { Jumbotron } from 'reactstrap';
 import { AuthContext } from '../App';
 import { Link } from 'react-router-dom';
+import './CSS/Home.css'
 
 function HomeComp() {
     const { state, dispatch } = useContext(AuthContext)
     return (
         <div>
-            <Jumbotron className="home">
-                <h1 className="display-3">Hello, {state.user}</h1>
-                <h2>Selamat Datang di Website KuyProperty</h2>
-                <p>Sudah punya akun ? <Link to="/login">Login</Link></p>               
-            </Jumbotron>
+            <div className="home">
+                <div className="display1">
+                    <h1 >Hello, {state.user}</h1>
+                    <h2>Selamat Datang di Website KuyProperty</h2>
+                </div>
+                <p className="display2">Sudah punya akun ? <Link className="linkto" to="/login">Login</Link></p>
+            </div>
         </div>
     )
 }
